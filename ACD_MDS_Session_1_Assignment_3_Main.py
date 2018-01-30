6a58c0aca02afd35c34ccd951f92fda3f9f135c8
hrd2910 <35139001+hrd2910@users.noreply.github.com>
2018-01-30 22:24:50 +0530

    Add files via upload

---
+++ ACD_MDS_Session_1_Assignment_3_Main.py
@@ -0,0 +1,9 @@
+
+
+# Write a Python program to accept the user's first and last name and then getting them printed in the the reverse order with a space between first name and last name.
+
+
+s='Hridi Ranjan Deb'
+print(s)
+s[::-1]
+
